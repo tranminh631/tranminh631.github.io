dacf2f96e97e0acc99ea6b236308e19c52af839b
Minh Tran <minh.lootting@gmail.com>
2020-07-15 14:46:22 -0400

    more info in skills

--- src/Skills.js
+++ src/Skills.js
@@ -4,10 +4,13 @@ import './Skills.css'
 function Skills() {
 	return(
 		<div className="Skills module">
-			<h2>Skills</h2>
-			<p><b>React</b> web developer with responsive experience</p>
-			<p className="note">Note: Please feel free to resize this page or visit via phone or tablet.</p>
+			<h2>Skills & Certifications</h2>
+			<p><b>React</b> web developer with responsive experience
+				<p className="note">Note: Please feel free to resize this page or visit via phone or tablet.</p>
+			</p>
 			<p><b>Java</b> developer in test. (Examples incoming)</p>
+			<p>ETL, Integration Testing, Cypress, Selenium, Cucumber, Scrum, AWS</p>
+			<p><b>ISTQB Certified Tester</b> - 18-CTFL-02026-USA</p>
 		</div>
 	)
 }
