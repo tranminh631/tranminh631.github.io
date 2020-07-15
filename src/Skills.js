d43620b0576f95ef274d36773cb22519b143b956
Minh Tran <minh.lootting@gmail.com>
2020-07-15 12:08:45 -0400

    initial commit

---
+++ src/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Skills.css'
+
+function Skills() {
+	return(
+		<div className="Skills module">
+			<h2>Skills</h2>
+			<p><b>React</b> web developer with responsive experience</p>
+			<p className="note">Note: Please feel free to resize this page or visit via phone or tablet.</p>
+			<p><b>Java</b> developer in test. (Examples incoming)</p>
+		</div>
+	)
+}
+
+export default Skills;
---
+++ src/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Welcome.css'
+
+function Welcome() {
+	return (
+		<div className="Welcome module">
+			<p>Welcome to Minh's Website</p>
+			<p>The premier place for information about Minh Tran</p>
+			<p>Software Developer from Herndon, VA</p>
+			<p>This website is built using <b>React</b></p>
+		</div>
+	)
+}
+
+export default Welcome;
