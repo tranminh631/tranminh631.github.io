8ab10f2cea87aa772b9df481ad7698f898575471
Minh Tran <minh.lootting@gmail.com>
2020-07-21 15:52:30 -0400

    certification update

--- src/Blog.js
+++ src/Blog.js
@@ -5,12 +5,12 @@ function Blog() {
 	return(
 		<div className="Blog module">
 			<h2>What am I up to?</h2>
-			<b>Last updated 7/15/2020</b>
+			<b>Last updated 7/21/2020</b>
 			<p>Constructing large website for side business with <b>React, Express, DynamoDB</b> stack</p>
 			<i className="fas fa-hammer"></i>
 			<p>
 				Getting more certifications. <b>
-				AWS Cloud Practitioner</b>, <b>Google Adwords</b>, and <b>Security+</b> on the horizon.
+				AWS Cloud Practitioner</b> and <b>Security+</b> on the horizon.
 			</p>
 			<i className="fas fa-book-open"></i>
 		</div>
--- src/Skills.js
+++ src/Skills.js
@@ -10,6 +10,9 @@ function Skills() {
 			<p><b>Java</b> developer in test. (Examples incoming)</p>
 			<p>ETL, Integration Testing, Cypress, Selenium, Express, Cucumber, Scrum, AWS</p>
 			<p><b>ISTQB Certified Tester</b> - 18-CTFL-02026-USA</p>
+			<p><a href="https://skillshop.exceedlms.com/profiles/08f10ae5d7c9405b848a19d36e0affe0"
+				target="_blank" rel="noopener noreferrer">
+				Google Ads Search Certification</a></p>
 		</div>
 	)
 }
